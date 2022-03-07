65ced6a2aa8bcd6a0bae1cffb968c1bec41c31b1
Shoeib Shargo <shargo3554@gmail.com>
2022-03-08 00:30:17 +0600

    added user.json file

--- src/test/java/LoginTestRunner.java
+++ src/test/java/LoginTestRunner.java
@@ -1,10 +1,26 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LoginTestRunner extends Setup{
     @Test
-    public void loginTest(){
+    public void loginTest() throws IOException, ParseException {
         Login login = new Login(driver);
-        login.doLogin("test_viva@test.com", "123456");
+        JSONParser jsonParser = new JSONParser();
+        Object obj = jsonParser.parse(new FileReader("./src/test/resources/user.json"));
+        JSONObject jsonObject = (JSONObject) obj;
+
+        String email = (String) jsonObject.get("email");
+        String password = (String) jsonObject.get("password");
+
+        String user = login.doLogin(email, password);
+        Assert.assertEquals(user, "viva test");
 
     }
 }
